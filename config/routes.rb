e8febcbe30616c023a43507bb98b1613e8c2032f
nhatkhuat <nhatkhuat8a@gmail.com>
2026-05-24 17:49:05 +0700

    Implement account activation feature with user mailer and controller updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -7,7 +8,9 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help", as: 'help'
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+
   resources :users
+  resources :account_activations, only: [:edit]
   
-  root "static_pages#home"
+
 end
